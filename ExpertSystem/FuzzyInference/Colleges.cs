f930dd2b2a1bfde052f73555dd865c88d247b8b6
Jarosław Węgliński <j.weglinski2@gmail.com>
2020-11-28 14:07:53 +0100

    fix limits

--- ExpertSystem/FuzzyInference/Colleges.cs
+++ ExpertSystem/FuzzyInference/Colleges.cs
@@ -8,7 +8,7 @@ namespace FuzzyInference
 {
     public static class Colleges
     {
-        public static (string name, double prestige ) PW = ("Politechnika Warszawska",78.9);
+        public static (string name, double prestige) PW = ("Politechnika Warszawska",78.9);
         public static (string name, double prestige) UW = ("Uniwersytet Warszawski",95.6);
         public static (string name, double prestige) AGH = ("Akademia Górniczo-Hutnicza",77.2);
         public static (string name, double prestige) UJ = ("Uniwersytet Jagielloński",100);
