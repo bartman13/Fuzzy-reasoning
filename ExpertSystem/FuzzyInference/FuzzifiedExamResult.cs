498daf01ecac3d0ef516d3c05291befe00f4d58b
teodorstupnicki <teodor.stupnicki@gmail.com>
2020-11-28 15:49:02 +0100

    fuzzification added

--- ExpertSystem/FuzzyInference/FuzzifiedExamResult.cs
+++ ExpertSystem/FuzzyInference/FuzzifiedExamResult.cs
@@ -10,6 +10,26 @@ namespace FuzzyInference
     {
         public ExamName ExamName { get; set; }
         public ExamLevel ExamLevel { get; set; }
-        public int Result { get; set; }
+
+        public double Low { get; set; }
+        public double Average { get; set; }
+        public double High { get; set; }
+
+        public FuzzifiedExamResult(ExamResult x)
+        {
+            int percentage = x.Result;
+            Average = 1 - Math.Abs(75 - percentage)/35;
+            if (percentage > 50)
+                High = 1 - Math.Abs(100 - percentage) / 50;
+            else
+                High = 0;
+            if (percentage < 80)
+                Low = 1 - (percentage - 30) / 50;
+            else
+                Low = 0;
+            ExamName = x.ExamName;
+            ExamLevel = x.ExamLevel;
+        }
+
     }
 }
--- ExpertSystem/FuzzyInference/InferenceEngine.cs
+++ ExpertSystem/FuzzyInference/InferenceEngine.cs
@@ -8,9 +8,24 @@ namespace FuzzyInference
 {
     public class InferenceEngine
     {
+        List<Rule> rules = new List<Rule>();
+
         public void Run(List<ExamResult> results)
         {
-
+            List<FuzzifiedExamResult> fuzzifiedResults = Fuzzify(results);
         }
+
+        public List<FuzzifiedExamResult> Fuzzify(List<ExamResult> results)
+        {
+            List<FuzzifiedExamResult> list = new List<FuzzifiedExamResult>();
+            foreach (ExamResult result in results)
+                list.Add(new FuzzifiedExamResult(result));
+            return list;
+        }
+    }
+
+    public class Rule
+    {
+
     }
 }
